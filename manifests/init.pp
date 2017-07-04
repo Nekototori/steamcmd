212bb55424aba4d83d068be09f5fa55b82259734
Nekototori <jessykah.bird@gmail.com>
2017-07-04 00:07:39 -0500

    Updated path to param and clarified location

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,18 +1,19 @@
 define steamcmd (
   $ensure = 'present',
   $app_id,
+  $steamcmd_path = 'C:/programdata\chocolatey\bin\',
 ) {
   if $ensure == 'present' {
     exec { "Installing ${title} via SteamCMD":
-      command => "cmd.exe /c steamcmd +login anonymous +app_update ${app_id} +quit",
-      unless  => "cmd.exe /c steamcmd +login anonymous +apps_installed +quit | findstr ${app_id}",
-      path    => 'C:/Windows/System32',
+      command => "steamcmd.exe +login anonymous +app_update ${app_id} +quit",
+      unless  => "steamcmd.exe +login anonymous +apps_installed +quit | findstr ${app_id}",
+      path    => $steamcmd_path,
     }
   } elsif $ensure == 'absent' {
     exec { "Removing ${title} via SteamCMD":
-      command => "cmd.exe /c steamcmd +login anonymous +app_uninstall ${app_id} +quit",
-      unless  => "cmd.exe /c steamcmd +login anonymous +apps_installed +quit | findstr ${app_id}",
-      path    => 'C:/Windows/System32',
+      command => "steamcmd.exe +login anonymous +app_uninstall ${app_id} +quit",
+      unless  => "steamcmd.exe +login anonymous +apps_installed +quit | findstr ${app_id}",
+      path    => $steamcmd_path,
     }
   }
 }
