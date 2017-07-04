fa8a61ccb5fe834dde3363cab767e118232a96e7
Nekototori <jessykah.bird@gmail.com>
2017-07-04 00:02:01 -0500

    Provided path

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,11 +6,13 @@ define steamcmd (
     exec { "Installing ${title} via SteamCMD":
       command => "cmd.exe /c steamcmd +login anonymous +app_update ${app_id} +quit",
       unless  => "cmd.exe /c steamcmd +login anonymous +apps_installed +quit | findstr ${app_id}",
+      path    => 'C:/Windows/System32',
     }
   } elsif $ensure == 'absent' {
     exec { "Removing ${title} via SteamCMD":
       command => "cmd.exe /c steamcmd +login anonymous +app_uninstall ${app_id} +quit",
       unless  => "cmd.exe /c steamcmd +login anonymous +apps_installed +quit | findstr ${app_id}",
+      path    => 'C:/Windows/System32',
     }
   }
 }
