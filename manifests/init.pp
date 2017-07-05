55e824219ec4ba864b4294af91467004cf97c0bf
Nekototori <jessykah.bird@gmail.com>
2017-07-04 21:11:19 -0500

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,18 +2,19 @@ define steamcmd (
   $ensure = 'present',
   $app_id,
   $steamcmd_path = 'C:/programdata/chocolatey/bin/',
+  $install_dir = "%programfiles(x86)%/Steam/steamapps/common/${title}",
 ) {
   if $ensure == 'present' {
     exec { "Installing ${title} via SteamCMD":
-      command => "steamcmd.exe +login anonymous +app_update ${app_id} +quit",
-      unless  => "steamcmd.exe +login anonymous +apps_installed +quit | findstr ${app_id}",
+      command => "steamcmd.exe +login anonymous +force_install_dir ${install_dir} +app_update ${app_id} +quit",
+      unless  => "if exist \"%${install_dir}%\" exit 1",
       path    => $steamcmd_path,
       timeout => 0,
     }
   } elsif $ensure == 'absent' {
     exec { "Removing ${title} via SteamCMD":
       command => "steamcmd.exe +login anonymous +app_uninstall ${app_id} +quit",
-      unless  => "steamcmd.exe +login anonymous +apps_installed +quit | findstr ${app_id}",
+      unless  => "if exist \"%${install_dir}%\" exit 0",
       path    => $steamcmd_path,
     }
   }
