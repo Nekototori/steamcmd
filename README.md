129eddb84f2e832a3258e5034c9ed091e0c3dcfc
Jessykah Bird <jessykah@puppet.com>
2017-07-05 11:42:14 -0500

    Updated documentation

--- README.md
+++ README.md
@@ -11,3 +11,10 @@ steamcmd { 'The Lab':
 
 Currently only works with app ids.  No proper API to do db lookups for
 matching by name.
+
+It will install it in the %programfiles(x86)%/Steam/steamapps/common/
+directory unless specified otherwise, with the title of the declaration
+becoming the folder name.
+By default it uses anonymous authentication, so if you wish to login,
+you will need to set anonymous to false and also supply a username and
+password.
--- manifests/init.pp
+++ manifests/init.pp
@@ -3,18 +3,33 @@ define steamcmd (
   $app_id,
   $steamcmd_path = 'C:/programdata/chocolatey/bin/',
   $install_dir = "%programfiles(x86)%/Steam/steamapps/common/${title}",
+  $username = undef,
+  $password = undef,
+  $anonymous_login = true,
 ) {
+
+# Determine login method.
+  if $anonymous_login == true {
+    $login = 'anonymous'
+  } else {
+    if ($username == undef) or ($password == undef) {
+      fail("When disabling anonymous login, username and password must be specified")
+    } else {
+      $login = "$username + ' ' + $password"
+    }
+  }
+
   if $ensure == 'present' {
     exec { "Installing ${title} via SteamCMD":
-      command => "steamcmd.exe +login anonymous +force_install_dir ${install_dir} +app_update ${app_id} +quit",
-      unless  => "cmd.exe /c if exist \"%${install_dir}%\" exit 1",
+      command => "steamcmd.exe +login ${login} +force_install_dir ${install_dir} +app_update ${app_id} +quit",
+      unless  => "cmd.exe /c if not exist \"%${install_dir}%\" exit 1",
       path    => [$steamcmd_path, 'C:/Windows/System32'],
       timeout => 0,
     }
   } elsif $ensure == 'absent' {
     exec { "Removing ${title} via SteamCMD":
-      command => "steamcmd.exe +login anonymous +app_uninstall ${app_id} +quit",
-      unless  => "cmd.exe /c if exist \"%${install_dir}%\" exit 0",
+      command => "steamcmd.exe +login ${login} +app_uninstall ${app_id} +quit",
+      unless  => "cmd.exe /c if exist \"%${install_dir}%\" exit 1",
       path    => [$steamcmd_path, 'C:/Windows/System32'],
     }
   }
